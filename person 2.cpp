a840405e6d620100d358de185a98335863c6dc72
3msayed <hhmahmoud826@gmail.com>
2026-05-04 21:44:40 +0300

    Add files via upload
    files bta3ty ana (khalaf) w dana

---
+++ person 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Khod el goz2 da bs
+// e3ml el global array b 2SM ( rooms ) zy ma ana 3aml
+
+void initializeRooms() {
+    rooms[0] = {101, "Single", 50.0, true};
+    rooms[1] = {102, "Single", 50.0, true};
+    rooms[2] = {201, "Double", 80.0, true};
+    rooms[3] = {202, "Double", 80.0, true};
+    rooms[4] = {501, "Suite", 150.0, true};
+}
+
+
+
+
+
